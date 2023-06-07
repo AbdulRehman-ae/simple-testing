b94e40bf80db3381799e4e357c037c79e0945c9d
ABDULREHMAN <135855370+AbdulRehman-ae@users.noreply.github.com>
2023-06-08 00:26:54 +0500

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+/*int len (char var [])
+{
+	int temp = strlen (var);
+	cout << temp;
+}
+int main ()
+{
+	char str [20];
+	
+	cin >> str;
+	len (str);
+}*/
+int main ()
+{
+	char str [20];
+	
+	cin >> str;
+	
+	cout << strlen (str);
+}
